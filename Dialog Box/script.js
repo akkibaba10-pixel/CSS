db4ffe2fdeb81ad4e6fa36387963579984bd4573
Aklesh <akkibaba10@gmail.com>
2026-01-20 15:39:17 +0530

    Add Closing and Opening Animation to Dialog Box

--- Dialog Box/script.js
+++ Dialog Box/script.js
@@ -1,8 +1,28 @@
-const loginDialog = document.getElementById("login-dialog");
-const signupDialog = document.getElementById("signup-dialog");
+// const loginDialog = document.getElementById("login-dialog");
+// const signupDialog = document.getElementById("signup-dialog");
 
-const showLoginDialog = (show) => showDialog(show, loginDialog);
-const showSignupDialog = (show) => showDialog(show, signupDialog);
+// const showLoginDialog = (show) => showDialog(show, loginDialog);
+// const showSignupDialog = (show) => showDialog(show, signupDialog);
 
-const showDialog = (show, dialog) =>
-  show ? dialog.showModal() : dialog.close();
+// const showDialog = (show, dialog) =>
+//   show ? dialog.showModal() : dialog.close();
+
+
+document.querySelectorAll(".open-modal").forEach(btn=>{
+  btn.addEventListener("click", ()=>{
+    let dialogId = btn.getAttribute("data-modal");
+    let dialog = document.getElementById(dialogId);
+    dialog.showModal();
+    dialog.querySelectorAll(".close-modal").forEach(btn=>{
+      btn.addEventListener("click", ()=>{
+        dialog.close();
+      })
+    })
+  })
+})
+// document.querySelectorAll(".close-modal").forEach(btn=>{
+//   btn.addEventListener("click", ()=>{
+//     let dialog = btn.closest("dialog");
+//     dialog.close();
+//   })
+// })
